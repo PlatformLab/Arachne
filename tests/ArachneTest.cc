c8eaf2fdc6fdc2f5133d7d719b4d93b33bd5e74c
Henry Qin <hq6@stanford.edu>
2016-10-16 15:53:01 -0700

    Make tests isomorphic to code.

--- tests/ArachneTest.cc
+++ tests/ArachneTest.cc
@@ -32,37 +32,39 @@ struct ArachneTest : public ::testing::Test {
     }
 };
 
-void blocker() {
-    Arachne::block();
-}
-
-TEST_F(ArachneTest, Signal) {
-    // We use a malloc here because we have deleted the constructor for
-    // ThreadContext.
-    ThreadContext *tempContext =
-        reinterpret_cast<ThreadContext*>(
-                malloc(sizeof(Arachne::ThreadContext)));
-    tempContext->generation = 0;
-    tempContext->wakeupTimeInCycles = ~0L;
-    Arachne::signal(ThreadId(tempContext, 0));
-    EXPECT_EQ(0, tempContext->wakeupTimeInCycles);
-}
-
-TEST_F(ArachneTest, AwakenBlockedThread) {
-    Arachne::ThreadId id = Arachne::createThread(0, blocker);
-    EXPECT_EQ(1, Arachne::occupiedAndCount[0].load().numOccupied);
-    EXPECT_EQ(1, Arachne::occupiedAndCount[0].load().occupied);
-
-    usleep(1000);
-    Arachne::signal(id);
-    while (Arachne::occupiedAndCount[0].load().numOccupied == 1);
-    EXPECT_EQ(0, Arachne::occupiedAndCount[0].load().occupied);
-}
-
 static Arachne::SpinLock mutex;
 static Arachne::ConditionVariable cv;
 static volatile int numWaitedOn;
+static volatile int flag;
 
+// Helper function for SpinLock tests
+static void lockTaker() {
+    flag = 1;
+    mutex.lock();
+    mutex.unlock();
+    flag = 0;
+}
+
+TEST_F(ArachneTest, spinLock_exclusion) {
+    flag = 0;
+    mutex.lock();
+    createThread(0, lockTaker);
+    while (!flag);
+    EXPECT_EQ(1, flag);
+    mutex.unlock();
+    while (flag);
+    EXPECT_EQ(0, flag);
+}
+
+TEST_F(ArachneTest, spinLock_tryLock) {
+    mutex.lock();
+    EXPECT_FALSE(mutex.try_lock());
+    mutex.unlock();
+    EXPECT_TRUE(mutex.try_lock());
+    mutex.unlock();
+}
+
+// Helper function for condition variable tests.
 static void waiter() {
     mutex.lock();
     while (!numWaitedOn)
@@ -70,10 +72,11 @@ static void waiter() {
     numWaitedOn--;
     mutex.unlock();
 }
-TEST_F(ArachneTest, NotifyOne) {
+
+TEST_F(ArachneTest, conditionVariable_notifyOne) {
     numWaitedOn = 0;
-    Arachne::createThread(0, waiter);
-    Arachne::createThread(0, waiter);
+    createThread(0, waiter);
+    createThread(0, waiter);
     EXPECT_EQ(2, Arachne::occupiedAndCount[0].load().numOccupied);
     EXPECT_EQ(3, Arachne::occupiedAndCount[0].load().occupied);
     numWaitedOn = 2;
@@ -91,11 +94,11 @@ TEST_F(ArachneTest, NotifyOne) {
     EXPECT_EQ(0, numWaitedOn);
 }
 
-TEST_F(ArachneTest, NotifyAll) {
+TEST_F(ArachneTest, conditionVariable_notifyAll) {
     mutex.lock();
     numWaitedOn = 0;
     for (int i = 0; i < 10; i++)
-        Arachne::createThread(0, waiter);
+        createThread(0, waiter);
     numWaitedOn = 5;
     cv.notifyAll();
     mutex.unlock();
@@ -107,6 +110,7 @@ TEST_F(ArachneTest, NotifyAll) {
     mutex.unlock();
 }
 
+// Helper functions for thread creation tests.
 static volatile int threadCreationIndicator = 0;
 
 void clearFlag() {
@@ -119,7 +123,7 @@ void setFlagForCreation(int a) {
     threadCreationIndicator = a;
 }
 
-TEST_F(ArachneTest, NoArgs) {
+TEST_F(ArachneTest, createThread_noArgs) {
     // By assigning the number of cores, we can run this test independently of
     // the number of cores on the machine.
     Arachne::numCores = 2;
@@ -127,7 +131,7 @@ TEST_F(ArachneTest, NoArgs) {
     Arachne::threadInit();
     EXPECT_EQ(0, Arachne::occupiedAndCount[0].load().numOccupied);
     EXPECT_EQ(0, Arachne::occupiedAndCount[0].load().occupied);
-    Arachne::createThread(0, clearFlag);
+    createThread(0, clearFlag);
 
     // This test may be a little fragile since it depends on the internal
     // structure of std::function
@@ -144,12 +148,12 @@ TEST_F(ArachneTest, NoArgs) {
     EXPECT_EQ(0, Arachne::occupiedAndCount[0].load().occupied);
 }
 
-TEST_F(ArachneTest, WithArgs) {
+TEST_F(ArachneTest, createThread_withArgs) {
     // By assigning the number of cores, we can run this test independently of
     // the number of cores on the machine.
     Arachne::numCores = 2;
     Arachne::threadInit();
-    Arachne::createThread(0, setFlagForCreation, 2);
+    createThread(0, setFlagForCreation, 2);
     EXPECT_EQ(1, Arachne::occupiedAndCount[0].load().numOccupied);
     EXPECT_EQ(1, Arachne::occupiedAndCount[0].load().occupied);
     EXPECT_EQ(0, threadCreationIndicator);
@@ -159,14 +163,14 @@ TEST_F(ArachneTest, WithArgs) {
     threadCreationIndicator = 0;
 }
 
-TEST_F(ArachneTest, MaxThreadsExceeded) {
+TEST_F(ArachneTest, createThread_maxThreadsExceeded) {
     // By assigning the number of cores, we can run this test independently of
     // the number of cores on the machine.
     Arachne::numCores = 2;
     Arachne::threadInit();
     for (int i = 0; i < Arachne::maxThreadsPerCore; i++)
-        EXPECT_NE(Arachne::NullThread, Arachne::createThread(0, clearFlag));
-    EXPECT_EQ(Arachne::NullThread, Arachne::createThread(0, clearFlag));
+        EXPECT_NE(Arachne::NullThread, createThread(0, clearFlag));
+    EXPECT_EQ(Arachne::NullThread, createThread(0, clearFlag));
 
     // Clean up the threads
     while (Arachne::occupiedAndCount[0].load().numOccupied > 0)
@@ -174,74 +178,20 @@ TEST_F(ArachneTest, MaxThreadsExceeded) {
     threadCreationIndicator = 0;
 }
 
-static Arachne::ThreadId joineeId;
+extern std::vector<void*> kernelThreadStacks;
 
-void joinee() {
-    EXPECT_LE(1, Arachne::occupiedAndCount[0].load().numOccupied);
+void threadMainHelper() {
+    swapcontext(&kernelThreadStacks[kernelThreadId], &runningContext->sp);
 }
 
-void joiner() {
-    Arachne::join(joineeId);
-    EXPECT_EQ(1, Arachne::occupiedAndCount[0].load().numOccupied);
-}
-
-void joinee2() {
-    Arachne::yield();
-}
-
-TEST_F(ArachneTest, JoinAfterTermination) {
-    Arachne::numCores = 2;
-    Arachne::threadInit();
-
-    // Since the joinee does not yield, we know that it terminated before the
-    // joiner got a chance to run.
-    joineeId = Arachne::createThread(0, joinee);
-    Arachne::createThread(0, joiner);
-
-    // Wait for threads to finish so that tests do not interfere with each
-    // other.
-    while (Arachne::occupiedAndCount[0].load().numOccupied > 0);
-}
-
-TEST_F(ArachneTest, JoinDuringRun) {
-    Arachne::numCores = 2;
-    Arachne::threadInit();
-    joineeId = Arachne::createThread(0, joinee2);
-    Arachne::createThread(0, joiner);
-    while (Arachne::occupiedAndCount[0].load().numOccupied > 0);
-}
-
-using PerfUtils::Cycles;
-
-static volatile int flag;
-
-void sleeper() {
-    uint64_t before = Cycles::rdtsc();
-    Arachne::sleep(1000);
-    uint64_t delta = Cycles::toNanoseconds(Cycles::rdtsc() - before);
-    EXPECT_LE(1000, delta);
-}
-
-void simplesleeper() {
-    Arachne::sleep(10000);
-    flag = 1;
-    while (flag);
-}
-
-TEST_F(ArachneTest, MinimumDelay) {
-    Arachne::numCores = 2;
-    Arachne::threadInit();
-    Arachne::createThread(0, sleeper);
-}
-
-TEST_F(ArachneTest, WakeupTimeSetAndCleared) {
-    Arachne::numCores = 2;
-    Arachne::threadInit();
-    flag = 0;
-    Arachne::createThread(0, simplesleeper);
-    while (!flag);
-    EXPECT_EQ(~0L, Arachne::activeLists[0]->wakeupTimeInCycles);
-    flag = 0;
+TEST_F(ArachneTest, threadMainFunction) {
+    createThread(1, threadMainHelper);
+    threadMainFunction(1);
+    EXPECT_EQ(activeList, activeLists[kernelThreadId]);
+    EXPECT_EQ(localOccupiedAndCount, occupiedAndCount + kernelThreadId);
+    EXPECT_EQ(1, localOccupiedAndCount->load().numOccupied);
+    // Manually clean up the state that the schedulerMainLoop would do.
+    *localOccupiedAndCount = {0, 0};
 }
 
 static const size_t testStackSize = 256;
@@ -256,7 +206,7 @@ void swapContextHelper() {
     Arachne::swapcontext(&oldStackPointer, &stackPointer);
 }
 
-TEST_F(ArachneTest, SwapContext) {
+TEST_F(ArachneTest, swapContext) {
     swapContextSuccess = 0;
     stackPointer = stack + testStackSize;
     *reinterpret_cast<void**>(stackPointer) =
@@ -269,6 +219,17 @@ TEST_F(ArachneTest, SwapContext) {
             reinterpret_cast<char*>(stack));
     Arachne::swapcontext(&stackPointer, &oldStackPointer);
     EXPECT_EQ(1, swapContextSuccess);
+}
+
+// Helper method for schedulerMainLoop
+void checkSchedulerState() {
+    EXPECT_EQ(~0L, runningContext->wakeupTimeInCycles);
+    EXPECT_EQ(1, localOccupiedAndCount->load().numOccupied);
+    EXPECT_EQ(1, localOccupiedAndCount->load().occupied);
+}
+
+TEST_F(ArachneTest, schedulerMainLoop) {
+    createThread(0, checkSchedulerState);
 }
 
 static volatile int keepYielding;
@@ -289,60 +250,128 @@ static void bitSetter(int index) {
     }
 }
 
-TEST_F(ArachneTest, SecondThreadGotControl) {
-    Arachne::numCores = 2;
-    Arachne::threadInit();
+TEST_F(ArachneTest, yield_secondThreadGotControl) {
+    numCores = 2;
+    threadInit();
     keepYielding = true;
-    Arachne::createThread(0, yielder);
+    createThread(0, yielder);
 
     flag = 0;
-    Arachne::createThread(0, setFlag);
+    createThread(0, setFlag);
     while (Arachne::occupiedAndCount[0].load().numOccupied > 1);
     EXPECT_EQ(1, flag);
     flag = 0;
     keepYielding = false;
 }
 
-TEST_F(ArachneTest, AllThreadsRan) {
+TEST_F(ArachneTest, yield_allThreadsRan) {
     Arachne::numCores = 2;
     Arachne::threadInit();
     keepYielding = true;
     flag = 0;
 
-    Arachne::createThread(0, bitSetter, 0);
-    Arachne::createThread(0, bitSetter, 1);
-    Arachne::createThread(0, bitSetter, 2);
+    createThread(0, bitSetter, 0);
+    createThread(0, bitSetter, 1);
+    createThread(0, bitSetter, 2);
     usleep(100);
 
     keepYielding = false;
     EXPECT_EQ(7, flag);
 }
 
-static void lockTaker() {
-    flag = 1;
-    mutex.lock();
-    mutex.unlock();
-    flag = 0;
+using PerfUtils::Cycles;
+
+void sleeper() {
+    uint64_t before = Cycles::rdtsc();
+    Arachne::sleep(1000);
+    uint64_t delta = Cycles::toNanoseconds(Cycles::rdtsc() - before);
+    EXPECT_LE(1000, delta);
 }
 
-TEST_F(ArachneTest, Exclusion) {
+void simplesleeper() {
+    Arachne::sleep(10000);
+    flag = 1;
+    while (flag);
+}
+
+TEST_F(ArachneTest, sleep_minimumDelay) {
+    numCores = 2;
+    threadInit();
+    createThread(0, sleeper);
+}
+
+TEST_F(ArachneTest, sleep_wakeupTimeSetAndCleared) {
     Arachne::numCores = 2;
     Arachne::threadInit();
     flag = 0;
-    mutex.lock();
-    Arachne::createThread(0, lockTaker);
+    createThread(0, simplesleeper);
     while (!flag);
-    EXPECT_EQ(1, flag);
-    mutex.unlock();
-    while (flag);
-    EXPECT_EQ(0, flag);
+    EXPECT_EQ(~0L, Arachne::activeLists[0]->wakeupTimeInCycles);
+    flag = 0;
 }
 
-TEST_F(ArachneTest, TryLock) {
-    mutex.lock();
-    EXPECT_FALSE(mutex.try_lock());
-    mutex.unlock();
-    EXPECT_TRUE(mutex.try_lock());
-    mutex.unlock();
+void blocker() {
+    Arachne::block();
 }
+
+TEST_F(ArachneTest, blockSignal) {
+    Arachne::ThreadId id = createThread(0, blocker);
+    EXPECT_EQ(1, Arachne::occupiedAndCount[0].load().numOccupied);
+    EXPECT_EQ(1, Arachne::occupiedAndCount[0].load().occupied);
+
+    usleep(1000);
+    Arachne::signal(id);
+    while (Arachne::occupiedAndCount[0].load().numOccupied == 1);
+    EXPECT_EQ(0, Arachne::occupiedAndCount[0].load().occupied);
+}
+
+TEST_F(ArachneTest, signal) {
+    // We use a malloc here because we have deleted the constructor for
+    // ThreadContext.
+    ThreadContext *tempContext =
+        reinterpret_cast<ThreadContext*>(
+                malloc(sizeof(Arachne::ThreadContext)));
+    tempContext->generation = 0;
+    tempContext->wakeupTimeInCycles = ~0L;
+    Arachne::signal(ThreadId(tempContext, 0));
+    EXPECT_EQ(0, tempContext->wakeupTimeInCycles);
+}
+
+static Arachne::ThreadId joineeId;
+
+void joinee() {
+    EXPECT_LE(1, Arachne::occupiedAndCount[0].load().numOccupied);
+}
+
+void joiner() {
+    Arachne::join(joineeId);
+    EXPECT_EQ(1, Arachne::occupiedAndCount[0].load().numOccupied);
+}
+
+void joinee2() {
+    Arachne::yield();
+}
+
+TEST_F(ArachneTest, join_afterTermination) {
+    Arachne::numCores = 2;
+    Arachne::threadInit();
+
+    // Since the joinee does not yield, we know that it terminated before the
+    // joiner got a chance to run.
+    joineeId = createThread(0, joinee);
+    createThread(0, joiner);
+
+    // Wait for threads to finish so that tests do not interfere with each
+    // other.
+    while (Arachne::occupiedAndCount[0].load().numOccupied > 0);
+}
+
+TEST_F(ArachneTest, join_DuringRun) {
+    Arachne::numCores = 2;
+    Arachne::threadInit();
+    joineeId = createThread(0, joinee2);
+    createThread(0, joiner);
+    while (Arachne::occupiedAndCount[0].load().numOccupied > 0);
+}
+
 } // namespace Arachne
