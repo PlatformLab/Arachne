5f1cea16bbcb1f80dd67de841552a0d481ea6bf8
Henry Qin <hq6@stanford.edu>
2016-11-21 17:21:48 -0800

    Changing PerfUtils usage in Arachne to use an explicit relative path.
    We thereby avoid colliding with RAMCloud's version of PerfUtils source files.
    It seems like a good idea in general for libraries to not use -I because of
    potential file name conflicts with application source files.

--- Arachne.cc
+++ Arachne.cc
@@ -17,8 +17,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <thread>
-#include "TimeTrace.h"
-#include "Util.h"
+#include "../PerfUtils/TimeTrace.h"
+#include "../PerfUtils/Util.h"
 
 namespace Arachne {
 
