b317df68f622ac6389ea7305945174e67cf15543
Henry Qin <hq6@stanford.edu>
2016-12-12 19:45:10 -0800

    Updated docs

--- search/all_0.js
+++ search/all_0.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['arachne_20public_20api',['Arachne Public API',['../group__api.html',1,'']]],
-  ['arachne_3a_20towards_20core_2daware_20scheduling',['Arachne: Towards Core-Aware Scheduling',['../index.html',1,'']]]
+  ['arachne_20public_20api',['Arachne Public API',['../group__api.html',1,'']]]
 ];
