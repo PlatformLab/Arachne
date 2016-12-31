e0c4bb1b0b3bfa7f33e8b96bdd5f8314827733ec
Henry Qin <hq6@stanford.edu>
2016-12-30 16:30:17 -0800

    Updating website on Fri Dec 30 16:30:14 PST 2016 from commit ba0d171ce47d965cc322c64abfb0b080bb7b6e44.

--- search/all_0.js
+++ search/all_0.js
@@ -1,7 +1,6 @@
 var searchData=
 [
   ['arachne_20public_20api',['Arachne Public API',['../group__api.html',1,'']]],
-  ['arachne_3a_20towards_20core_2daware_20scheduling',['Arachne: Towards Core-Aware Scheduling',['../index.html',1,'']]],
   ['arachne_20kernel_20api_20design',['Arachne Kernel API Design',['../md_wiki_KernelAPI.html',1,'']]],
   ['arachne_20design_20wiki',['Arachne Design Wiki',['../md_wiki_Main.html',1,'']]]
 ];
--- search/pages_0.js
+++ search/pages_0.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['arachne_3a_20towards_20core_2daware_20scheduling',['Arachne: Towards Core-Aware Scheduling',['../index.html',1,'']]],
   ['arachne_20kernel_20api_20design',['Arachne Kernel API Design',['../md_wiki_KernelAPI.html',1,'']]],
   ['arachne_20design_20wiki',['Arachne Design Wiki',['../md_wiki_Main.html',1,'']]]
 ];
