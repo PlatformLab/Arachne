35a769a5b5acc7defee448988c458194a60ca1e3
Henry Qin <hq6@stanford.edu>
2017-02-11 16:26:36 -0800

    Added support for all backtraces on current core.

--- backtrace.py
+++ backtrace.py
@@ -10,26 +10,58 @@ class BackTraceArachneCommand (gdb.Command):
                          gdb.COMMAND_STACK,
                          gdb.COMPLETE_SYMBOL, True)
     gdb.execute("alias -a bta = backtrace-arachne", True)
+
+  def backtrace(self, threadContext, from_tty):
+
+    # Check if we are backtracing the current context
+    loadedContext = gdb.parse_and_eval("Arachne::loadedContext")
+    if isinstance(threadContext, basestring):
+        threadContext = gdb.parse_and_eval(threadContext)
+    if long(loadedContext) == long(threadContext):
+        gdb.execute("backtrace", from_tty)
+        return
+
+    originalSP = gdb.parse_and_eval("$sp")
+    originalPC = long(gdb.parse_and_eval("$pc"))
+    print "Original SP:", originalSP
+    print "Original PC:", originalPC
+    gdb.execute("set $rsp=((Arachne::ThreadContext*){0})->sp + Arachne::SpaceForSavedRegisters".format(threadContext), from_tty)
+    gdb.execute("set $pc = *(void **)$rsp", from_tty)
+    gdb.execute("backtrace", from_tty)
+    gdb.execute("set ($sp)={0}".format(originalSP), from_tty)
+    gdb.execute("set ($pc)={0}".format(originalPC), from_tty)
+
   def invoke(self, arg, from_tty):
     arg = arg.strip()
     if arg == "":
-        print "Please pass an Arachne::ThreadContext*"
+        # Backtrace all threadcontexts that are occupied in the current core
+        maskAndCountPointer = gdb.parse_and_eval("Arachne::localOccupiedAndCount")
+        if maskAndCountPointer == 0:
+          print "Current core is not an Arachne core!"
+          return
+        bitmask = maskAndCountPointer.dereference()['_M_i']['occupied']
+
+        # Perform a backtrace on all the occupied bits.
+        for i in xrange(56):
+           if (bitmask >> i) & 1:
+               threadContext = gdb.parse_and_eval("Arachne::localThreadContexts[{0}]".format(i))
+               print "Arachne Thread {0}: {1}".format(i, threadContext)
+               self.backtrace(threadContext, from_tty)
+
         return
+
     # Verify that the type is correct
     typestring=gdb.execute("whatis {0}".format(arg), from_tty, True)
     if typestring.strip() != "type = Arachne::ThreadContext *":
         print "Please pass an Arachne::ThreadContext*"
         return
 
-    # Actually perform backtrace
-    originalSP = gdb.parse_and_eval("$sp")
-    originalPC = str(gdb.parse_and_eval("$pc")).split()[0]
-    print "Original SP:", originalSP
-    print "Original PC:", originalPC
-    gdb.execute("set $sp={0}->sp + Arachne::SpaceForSavedRegisters".format(arg), from_tty)
-    gdb.execute("set $pc = *(void **)$rsp", from_tty)
-    gdb.execute("backtrace", from_tty)
-    gdb.execute("set $sp={0}".format(originalSP), from_tty)
-    gdb.execute("set $pc={0}".format(originalPC), from_tty)
+    # Check if the provided threadcontext is NULL, and do nothing if it is.
+    threadcontextvalue = long(gdb.parse_and_eval(arg))
+    if threadcontextvalue == 0:
+        print "A NULL pointer was passed!"
+        return
+
+    self.backtrace(arg, from_tty)
 
 BackTraceArachneCommand()
