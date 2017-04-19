4472d5a4da141b46379c35b09e20e76110ee37b0
Henry Qin <hq6@stanford.edu>
2017-04-19 15:45:59 -0700

    Changed the PerfStat from number of times creation was contended to number of contended creations

--- PerfStats.cc
+++ PerfStats.cc
@@ -99,14 +99,14 @@ PerfStats::collectStats(PerfStats* total)
     for (PerfStats* stats : registeredStats) {
         // Note: the order of the statements below should match the
         // declaration order in PerfStats.h.
-        total->idleCycles           += stats->idleCycles;
-        total->totalCycles          += stats->totalCycles;
-        total->weightedLoadedCycles += stats->weightedLoadedCycles;
-        total->numThreadsCreated    += stats->numThreadsCreated;
-        total->numThreadsFinished   += stats->numThreadsFinished;
-        total->numCoreIncrements    += stats->numCoreIncrements;
-        total->numCoreDecrements    += stats->numCoreDecrements;
-        total->numTimesContended    += stats->numTimesContended;
+        total->idleCycles            += stats->idleCycles;
+        total->totalCycles           += stats->totalCycles;
+        total->weightedLoadedCycles  += stats->weightedLoadedCycles;
+        total->numThreadsCreated     += stats->numThreadsCreated;
+        total->numThreadsFinished    += stats->numThreadsFinished;
+        total->numCoreIncrements     += stats->numCoreIncrements;
+        total->numCoreDecrements     += stats->numCoreDecrements;
+        total->numContendedCreations += stats->numContendedCreations;
     }
 }
 }  // namespace Arachne
