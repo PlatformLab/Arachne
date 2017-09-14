6e21408e465d1ff0069f0d8c433f5197ced9c9fb
Henry Qin <hq6@stanford.edu>
2017-09-14 15:14:01 -0700

    Updating scripts to use the TLS variable core instead of using thread local varibles for each member

--- scripts/gdb-backtrace-arachne.py
+++ scripts/gdb-backtrace-arachne.py
@@ -14,7 +14,7 @@ class BackTraceArachneCommand (gdb.Command):
   def backtrace(self, threadContext, from_tty):
 
     # Check if we are backtracing the current context
-    loadedContext = gdb.parse_and_eval("Arachne::loadedContext")
+    loadedContext = gdb.parse_and_eval("Arachne::core.loadedContext")
     if isinstance(threadContext, basestring):
         threadContext = gdb.parse_and_eval(threadContext)
     if long(loadedContext) == long(threadContext):
@@ -29,16 +29,16 @@ class BackTraceArachneCommand (gdb.Command):
     r15 = long(gdb.parse_and_eval("$r15"))
     rbx = long(gdb.parse_and_eval("$rbx"))
     rbp = long(gdb.parse_and_eval("$rbp"))
-    loadedContext = long(gdb.parse_and_eval("Arachne::loadedContext"))
+    loadedContext = long(gdb.parse_and_eval("Arachne::core.loadedContext"))
 
-    gdb.execute("set Arachne::loadedContext = ((Arachne::ThreadContext*){0})".format(threadContext))
-    gdb.execute("set $rbp = *(uint64_t*) Arachne::loadedContext->sp")
-    gdb.execute("set $rbx = *(((uint64_t*) Arachne::loadedContext->sp)+1)")
-    gdb.execute("set $r15 = *(((uint64_t*) Arachne::loadedContext->sp)+2)")
-    gdb.execute("set $r14 = *(((uint64_t*) Arachne::loadedContext->sp)+3)")
-    gdb.execute("set $r13 = *(((uint64_t*) Arachne::loadedContext->sp)+4)")
-    gdb.execute("set $r12 = *(((uint64_t*) Arachne::loadedContext->sp)+5)")
-    gdb.execute("set $rsp=Arachne::loadedContext->sp + Arachne::SpaceForSavedRegisters", from_tty)
+    gdb.execute("set Arachne::core.loadedContext = ((Arachne::ThreadContext*){0})".format(threadContext))
+    gdb.execute("set $rbp = *(uint64_t*) Arachne::core.loadedContext->sp")
+    gdb.execute("set $rbx = *(((uint64_t*) Arachne::core.loadedContext->sp)+1)")
+    gdb.execute("set $r15 = *(((uint64_t*) Arachne::core.loadedContext->sp)+2)")
+    gdb.execute("set $r14 = *(((uint64_t*) Arachne::core.loadedContext->sp)+3)")
+    gdb.execute("set $r13 = *(((uint64_t*) Arachne::core.loadedContext->sp)+4)")
+    gdb.execute("set $r12 = *(((uint64_t*) Arachne::core.loadedContext->sp)+5)")
+    gdb.execute("set $rsp=Arachne::core.loadedContext->sp + Arachne::SpaceForSavedRegisters", from_tty)
     gdb.execute("set $pc = *(void **)$rsp", from_tty)
 
     gdb.execute("backtrace", from_tty)
@@ -52,13 +52,13 @@ class BackTraceArachneCommand (gdb.Command):
     gdb.execute("set $r14 = {0}".format(r14), from_tty)
     gdb.execute("set $r13 = {0}".format(r13), from_tty)
     gdb.execute("set $r12 = {0}".format(r12), from_tty)
-    gdb.execute("set Arachne::loadedContext = {0}".format(loadedContext))
+    gdb.execute("set Arachne::core.loadedContext = {0}".format(loadedContext))
 
   def invoke(self, arg, from_tty):
     arg = arg.strip()
     if arg == "":
         # Backtrace all threadcontexts that are occupied in the current core
-        maskAndCountPointer = gdb.parse_and_eval("Arachne::localOccupiedAndCount")
+        maskAndCountPointer = gdb.parse_and_eval("Arachne::core.localOccupiedAndCount")
         if maskAndCountPointer == 0:
           print "Current core is not an Arachne core!"
           return
@@ -67,7 +67,7 @@ class BackTraceArachneCommand (gdb.Command):
         # Perform a backtrace on all the occupied bits.
         for i in xrange(56):
            if (bitmask >> i) & 1:
-               threadContext = gdb.parse_and_eval("Arachne::localThreadContexts[{0}]".format(i))
+               threadContext = gdb.parse_and_eval("Arachne::core.localThreadContexts[{0}]".format(i))
                print "Arachne Thread {0}: {1}".format(i, threadContext)
                try:
                    self.backtrace(threadContext, from_tty)
