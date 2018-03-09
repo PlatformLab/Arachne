31f7761da3ac919c2040c9428777b19314076414
Qian Li <liqian.cs@gmail.com>
2018-03-08 19:42:11 -0700

    Manually register thread local perf stats in C wrapper arachne_init
    The main thread in C is controlled by C, and it doesn't run constructors for thread locals, which means the stats is not registered. This can cause incorrect aggregated data due to missing records in the main thread perf stats.
    
    We discovered this bug through counting thread creations.

--- cwrapper/arachne_wrapper.cc
+++ cwrapper/arachne_wrapper.cc
@@ -37,6 +37,13 @@ arachne_init(int* argcp, const char** argv) {
     // Capture the exception threw by Arachne::init
     try {
         Arachne::init(argcp, argv);
+
+        // Manually register thread local perf stats in C wrapper
+        // Reason: The main thread in C is controlled by C, and it doesn't run
+        // constructors for thread locals, which means the stats is not
+        // registered. This can cause incorrect aggregated data due to missing
+        // records in the main thread perf stats.
+        Arachne::PerfStats::registerStats(&Arachne::PerfStats::threadStats);
     } catch (const CoreArbiter::CoreArbiterClient::ClientException& e) {
         retval = -1;
         errno = ECONNREFUSED;  // Set errno to "Connection refused"
