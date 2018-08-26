e41f2db236217d64b6f17c6a04ad2265d0b49595
Qian Li <liqian.cs@gmail.com>
2018-08-26 11:50:09 -0700

    Fix the computation of localThreshold
    As disscussed, we should use the hysteresis as a relative threshold to avoid
    oscillation and ramp down issue.

--- src/CoreLoadEstimator.cc
+++ src/CoreLoadEstimator.cc
@@ -65,8 +65,9 @@ CoreLoadEstimator::estimate(CorePolicy::CoreList coreList) {
         // Scale down if the core utilization after scale down is greater than
         // the core utilization at which we scaled up, plus a hysteresis
         // threshold.
-        double localThreshold = utilizationThresholds[curActiveCores - 1] -
-                                idleCoreFractionHysteresis;
+        double localThreshold =
+            utilizationThresholds[curActiveCores - 1] -
+            idleCoreFractionHysteresis * (curActiveCores - 1);
 
         ARACHNE_LOG(DEBUG,
                     "curActiveCores = %d, totalUtilizedCores = %lf, "
