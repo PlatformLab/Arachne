484b206c7c672c97eb9bd90c3534fcfeb4db41c0
Henry Qin <hq6@stanford.edu>
2018-09-23 00:21:46 -0700

    Move allocation of occupied bitmask to main thread

--- src/Arachne.cc
+++ src/Arachne.cc
@@ -249,11 +249,6 @@ alignedAlloc(size_t size, size_t alignment) {
  */
 void
 initializeCore(Core* core) {
-    core->localOccupiedAndCount =
-        reinterpret_cast<std::atomic<Arachne::MaskAndCount>*>(
-            alignedAlloc(sizeof(std::atomic<MaskAndCount>)));
-    memset(core->localOccupiedAndCount, 0, sizeof(std::atomic<MaskAndCount>));
-
     core->localPinnedContexts = reinterpret_cast<std::atomic<uint64_t>*>(
         alignedAlloc(sizeof(uint64_t)));
     // All cores initially poll for work on context 0's stack.
@@ -278,7 +273,6 @@ initializeCore(Core* core) {
  */
 void
 deinitializeCore(Core* core) {
-    free(core->localOccupiedAndCount);
     free(core->localPinnedContexts);
     free(core->publicPriorityMask);
 
@@ -318,7 +312,7 @@ threadMain() {
             break;
         {
             std::lock_guard<SpinLock> _(coreChangeMutex);
-            occupiedAndCount[core.id] = core.localOccupiedAndCount;
+            core.localOccupiedAndCount = occupiedAndCount[core.id];
             pinnedContexts[core.id] = core.localPinnedContexts;
             allThreadContexts[core.id] = core.localThreadContexts;
             publicPriorityMasks[core.id] = core.publicPriorityMask;
@@ -876,6 +870,10 @@ waitForTermination() {
     // We now assume that all threads are done executing.
     PerfUtils::Util::serialize();
 
+    for (size_t i = 0; i < occupiedAndCount.size(); i++) {
+        free(occupiedAndCount[i]);
+    }
+
     kernelThreads.clear();
     kernelThreadStacks.clear();
 
@@ -1140,6 +1138,10 @@ init(int* argcp, const char** argv) {
     allThreadContexts.resize(numHardwareCores);
     coreIdle = new std::atomic<bool>[numHardwareCores];
     for (unsigned int i = 0; i < numHardwareCores; i++) {
+        occupiedAndCount[i] = reinterpret_cast<std::atomic<Arachne::MaskAndCount>*>(
+            alignedAlloc(sizeof(std::atomic<MaskAndCount>)));
+        memset(occupiedAndCount[i], 0, sizeof(std::atomic<MaskAndCount>));
+
         coreIdleSemaphores.push_back(new ::Semaphore);
         coreIdle[i].store(false);
     }
